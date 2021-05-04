7282d6f4a505a68e8c485062461e181ac8620afe
ChrisgKent <chrisgkent@gmail.com>
2021-05-05 00:02:40 +0100

    Added mutlicore processing of the counter function.

--- functions.R
+++ functions.R
@@ -1,10 +1,18 @@
 #Run this file 
 
 #This take a set of XStringSet, and returns the number times the sequnce appears.
-counter <- function(xstring, mismatch = 0){
+# mc arg. allows the use of mutlicore processing. Does throw a warning if the unique(xstring) isnt devisable by 10
+counter <- function(xstring, mismatch = 0, mc = FALSE){
   unique_x <- unique(xstring)
-  x <- vcountPDict(unique_x,xstring, collapse = 1, max.mismatch = mismatch)
+  if(mc == TRUE){
+    list <- split(unique_x, 1:10)
+    x <- mclapply(list, function(x){
+      vcountPDict(x,xstring, collapse = 1, max.mismatch = mismatch)})
+    dat <- DataFrame(seq = unlist(list), count = unlist(x))
+    
+  }else{x <- vcountPDict(unique_x,xstring, collapse = 1, max.mismatch = mismatch)
   dat <- DataFrame(seq = unique_x, count = x)
+  }
   rev(dat[(order(dat$count)),])
 }
 
@@ -19,3 +27,5 @@ vr_extract <- function(x){
   i <- x %>% Biostrings::subseq(start= 57, width = 36)
   Biostrings::translate(i)
 }
+
+
