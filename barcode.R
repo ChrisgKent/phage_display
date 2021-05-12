6b29ea6d7ada4adc07e60bdf0081159f3238578b
ChrisgKent <chrisgkent@gmail.com>
2021-05-12 21:42:15 +0100

    Using table() reduces computational load by 100x
    Much harder to use clusters/multicore with, however, the optomisation will be worth it.

--- barcode.R
+++ barcode.R
@@ -10,21 +10,29 @@ raw_fastq <- readFastq(file)
 #Extracts the sequence data from the file, as asigns it to a DNAStringSet
 raw_dat <- sread(raw_fastq) %>% DNAStringSet()
 
+
 #Extracts the section of the sequene where the barcode is expected.
 #Counter determins the number of time a single barcode pattern has been read
 barcodes <- Biostrings::subseq(raw_dat, start = 50, width = 6)
-barcode_count <- counter(barcodes)
 
+barcode_count <- table(barcodes) %>% 
+  as.data.frame() %>% 
+  arrange(by=desc(Freq))
+
+barcode_count <- DataFrame(seq = DNAStringSet(barcode_count[,1]), 
+                           freq = barcode_count[,2])
 
 write_delim(as.data.frame(barcode_count), "PRIVATE_DO_NOT_COMMIT/files/barcode_count.txt", col_names = FALSE)
 
+
 # This picks the 20 barcodes with the highest count, and assigns them to a list
 # As strings for memory reasons.
-top_20 <- barcode_count[1:200,]
+top_20 <- barcode_count[1:20,]
 seq_str <- top_20$seq %>% toString() %>% str_split(", ", simplify = TRUE)
 blist <- as.list(seq_str)
 names(blist) <- seq_str
 
-
 # This applys the barcode list to the raw_data file, and subsets sequences based on the barcodes
 bc_subset <- mclapply(blist, subseter) %>% DNAStringSetList()
+
+
