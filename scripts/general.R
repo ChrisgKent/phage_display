c2430b4df0b7587aeaa64c87f031c81a3736eaa5
ChrisgKent <chrisgkent@gmail.com>
2021-07-05 09:58:42 +0100

    Added a plot.
    Changed the output format
    
    Validated the results.

--- scripts/general.R
+++ scripts/general.R
@@ -7,7 +7,7 @@ library(ShortRead)
 
 #Reads in a Fastq file from the Dir
 file2 <- "/Users/chriskent/phage_seq/seq_2/R2/"
-input <- data.frame(file_dir = paste0(file2, "phagestockA_S10_L001_R2_001.fastq"),
+x <- data.frame(file_dir = paste0(file2, "phagestockA_S10_L001_R2_001.fastq"),
                     output_name = c("unpan"))
 
 counter <- function(x){
@@ -16,15 +16,18 @@ counter <- function(x){
     DNAStringSet() %>% 
     reverseComplement()
 
-  # Removes which do not contain sequence just before VR
-contains_vr <- vcountPattern(DNAString("TATTCTCACT"), raw_data2, max.mismatch = 1)
+# Removes which do not contain sequence just before VR
+contains_vr <- vcountPattern(DNAString("TATTCTCACTCT"), raw_data2, max.mismatch = 1)
 raw_data3 <- raw_data2[contains_vr == 1]
 
 # Identifys the most common location of the sequence
-vr_location <- vmatchPattern("TATTCTCACT", raw_data3) %>% unlist() 
+vr_location <- vmatchPattern("TATTCTCACTCT", raw_data3) %>% unlist() 
 vr_distro <- as.matrix(vr_location)[,1] %>% table()
 max_distro <- names(vr_distro)[vr_distro == max(vr_distro)] %>%
   as.numeric()
+
+
+
 
 # Generates the String to remove from left side, using the most common start + 11
 l_str <- consensusString(Biostrings::subseq(raw_data3, start = 1, width = max_distro + 11))
@@ -52,8 +55,17 @@ tabler <- function(x){
 }
 
 count_dat <- tabler(vr)
+
+
+# Recording results:
+dir <- paste0("/Users/chriskent/phage_seq/count_dat/", x$output_name)
+dir.create(dir)
+
 write_csv(data.frame(count_dat),
-          paste0("/Users/chriskent/phage_seq/count_dat/", x$output_name, "_autogen.csv"))
+          paste0(dir, "/", x$output_name, "_autogen.csv"))
+
+write_csv(data.frame(seq = cut, aa = translate(cut)),
+          paste0(dir, "/", "seq_", x$output_name, "_autogen.csv"))
 
 # Generates a log for the data
 log <- glue::glue("For {x$output_name}
@@ -64,7 +76,42 @@ log <- glue::glue("For {x$output_name}
            length: {length(test[width(test) == com_length])}
            {(length(test[width(test) == com_length])/length(raw_data2))*100}% sequences used")
 write_file(log, 
-           paste0("/Users/chriskent/phage_seq/count_dat/", x$output_name, "_log.csv"))
+           paste0(dir, "/", x$output_name, "_log.csv"))
 
+
+
+# plotting
+test3 <- consensusMatrix(raw_data2)  %>% .[1:4,] %>%
+  t() %>% as.data.frame() %>%
+  mutate(sum = rowSums(.)) %>%
+  mutate(position = 1:151) %>%
+  select(position, 1:5)
+
+test_p <- test3 %>% 
+  pivot_longer(2:5, names_to = "bases", values_to = "count") %>%
+  mutate(prop = count / sum)
+
+plot_freq <- ggplot(test_p, aes(position,prop, col = bases))+
+  geom_line()+
+  theme_bw()+
+  labs(title = paste0("Base Frequency of ", x$output_name),
+       x = "Base position",
+       y = "Proportion of each base")+
+  scale_x_continuous(breaks = seq(0,200,10))+
+  scale_color_discrete(name = "Bases")+
+  theme(plot.title = element_text(face = "bold", size = 16),
+        legend.title = element_text(face = "bold"),
+        legend.background = element_rect(colour = "grey"))
+
+ggplot2::ggsave(filename = paste0(dir, "/", x$output_name, "_freq"),
+                plot = plot_freq,
+                device = "png",
+                width = 10,
+                height = 5,
+                units = "in")
 }
+
+
 counter(input)
+
+
